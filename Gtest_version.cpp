629113275685d042450e28830a40ebecfbedd0ee
Pavel-Alyukaev <pavel-alyukaev@yandex.ru>
2020-01-15 21:50:08 +0700

    правка GMake для GTest

---
+++ Gtest_version.cpp
@@ -0,0 +1,23 @@
+
+#include <gtest/gtest.h>
+
+class Test_valid_version : public ::testing::Test {
+public:
+    Test_valid_version() {/* init protected members here */ }
+    ~Test_valid_version() {/* free protected members here */ }
+    void SetUp() {/* called before every test */ }
+    void TearDown() {/* called after every test */ }
+
+protected:
+    /*  none yet */
+};
+
+TEST_F(Test_valid_version, Version) {
+   ASSERT_GT(version(), 0); 
+}
+
+
+int main( int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+} 
