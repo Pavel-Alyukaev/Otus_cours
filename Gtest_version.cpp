eb92b89ed2d4530dc4ac6d58603ea9bc1433a272
Pavel-Alyukaev <pavel-alyukaev@yandex.ru>
2020-01-22 11:55:35 +0700

    исправления коментария

--- Gtest_version.cpp
+++ Gtest_version.cpp
@@ -1,17 +1,17 @@
 
 #include <gtest/gtest.h>
 #include "lib.h"
-/*
-class Test_valid_version : public ::testing::Test {
-public:
-    Test_valid_version() {/* init protected members here */ }
-    ~Test_valid_version() {/* free protected members here */ }
-    void SetUp() {/* called before every test */ }
-    void TearDown() {/* called after every test */ }
 
-protected:
+//class Test_valid_version : public ::testing::Test {
+//public:
+//    Test_valid_version() {/* init protected members here */ }
+//    ~Test_valid_version() {/* free protected members here */ }
+//    void SetUp() {/* called before every test */ }
+//    void TearDown() {/* called after every test */ }
+
+//protected:
     /*  none yet */
-};*/
+//};*/
 
 TEST_F(Test_valid_version, Version) {
    ASSERT_GT(1, 0); 
